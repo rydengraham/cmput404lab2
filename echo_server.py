a0ab2e2eb47507f296dc47518cd59b801ceb9d0e
rydengraham <ryden3.14@gmail.com>
2023-01-20 09:17:50 -0700

    Made echo server multithreaded

--- echo_server.py
+++ echo_server.py
@@ -1,4 +1,5 @@
 import socket
+from threading import Thread
 
 BYTES_TO_READ = 4096
 HOST = "127.0.0.1"
@@ -28,4 +29,20 @@ def start_server():
 
 	return
 
-start_server()
+
+def start_threaded_server():
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.bind((HOST,PORT))
+		s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+		s.listen()
+
+		while True:
+			conn, addr = s.accept()
+			thread = Thread(target=handle_connection, args=(conn, addr))
+			thread.run()
+
+	return
+
+#start_server()
+start_threaded_server()
